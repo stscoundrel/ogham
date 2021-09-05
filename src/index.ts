98e61d6a3a474127150e2db202b3d270317d3101
stscoundrel <sampo@pixels.fi>
2021-09-05 20:22:03 +0300

    Add Ogham to letters map & transform

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
-export const example = (): string => 'test string';
+import { getOghamMapping } from './mappings/ogham-mapping';
+
+const transform = (content: string, dictionary: Map<string, string>) : string => {
+  let result = '';
+  const parts: string[] = content.split('');
+
+  parts.forEach((part) => {
+    const partKey = part.toLocaleLowerCase();
+
+    if (dictionary.has(partKey)) {
+      result += dictionary.get(partKey);
+    } else {
+      result += part;
+    }
+  });
+
+  return result;
+};
+
+export const oghamToLetters = (content: string) : string => {
+  const oghamMapping = getOghamMapping();
+  const result = transform(content, oghamMapping);
+
+  return result;
+};
 
 export default {
-  example,
+  oghamToLetters,
+  getOghamMapping,
 };
---
+++ src/mappings/ogham-mapping.ts
@@ -0,0 +1,42 @@
+export const getOghamMapping = (): Map<string, string> => {
+  const oghamMapping = new Map();
+  // Sentence start & end should not be empty.
+  oghamMapping.set('᚛', '');
+  oghamMapping.set('᚜', '');
+
+  oghamMapping.set('ᚁ', 'b');
+  oghamMapping.set('ᚂ', 'l');
+  oghamMapping.set('ᚃ', 'v');
+  oghamMapping.set('ᚄ', 's');
+  oghamMapping.set('ᚅ', 'n');
+  oghamMapping.set('ᚆ', 'j');
+  oghamMapping.set('ᚇ', 'd');
+  oghamMapping.set('ᚈ', 't');
+  oghamMapping.set('ᚉ', 'c');
+  oghamMapping.set('ᚊ', 'q');
+  oghamMapping.set('ᚋ', 'm');
+  oghamMapping.set('ᚌ', 'g');
+  oghamMapping.set('ᚍ', 'ng');
+  oghamMapping.set('ᚎ', 'z');
+  oghamMapping.set('ᚏ', 'r');
+  oghamMapping.set('ᚐ', 'a');
+  oghamMapping.set('ᚑ', 'o');
+  oghamMapping.set('ᚒ', 'u');
+  oghamMapping.set('ᚓ', 'e');
+  oghamMapping.set('ᚔ', 'i');
+  oghamMapping.set('ᚕ', 'k');
+  oghamMapping.set('ᚖ', 'oi');
+  oghamMapping.set('ᚗ', 'ui');
+  oghamMapping.set('ᚘ', 'io');
+  oghamMapping.set('ᚙ', 'ai');
+  oghamMapping.set('ᚚ', 'p');
+
+  // space
+  oghamMapping.set(' ', ' ');
+
+  return oghamMapping;
+};
+
+export default {
+  getOghamMapping,
+};
